ca2494d7ba09439b8ca5bcc60fe124b1e2bdf85a
sted9000 <sted9000@gmail.com>
2023-11-17 00:03:01 -0500

    prepared for Heroku deploy

--- server/app.js
+++ server/app.js
@@ -12,16 +12,23 @@ app.use(cors()); // Dev only
 app.use(express.json()); // Parse JSON bodies
 
 // Connect to MongoDB
-const uri = process.env.MONGO_URI;
 mongoose
-  .connect(uri, {})
+  .connect(process.env.MONGO_URI, {})
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+// Serve Vue.js static files
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Handle SPA for Vue.js routing
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 // Routes
 let routes = require("./routes");
 app.use("/", routes);
 
 // Define the port
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Listening on port ${port}...`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
